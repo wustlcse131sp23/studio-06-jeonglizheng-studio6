638f0ada8c1a2a451f1bc4d7d10255da2399c939
86139 <86139@Yaxin-Li.dhcp.wustl.edu>
2023-03-09 15:14:00 -0600

    Studio 6 Finished!~

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -1,5 +1,7 @@
 package studio6;
 
+import java.util.Arrays;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
@@ -14,8 +16,12 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
-		
+		}
+		else {
+			return Math.pow(0.5, n) + geometricSum(n - 1);
+		}
 	}
 
 	/**
@@ -26,11 +32,21 @@ public class RecursiveMethods {
 	 * @param q second operand
 	 * @return greatest common divisor of p and q
 	 */
+	
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
+		if (q == 0) {
 			return 0;
-		
+		}
+		else {
+			if (p % q == 0) {
+				return q;
+			}
+			else {
+				return gcd(q, p % q);
+			}
+		}
 	}
 
 	
@@ -44,8 +60,36 @@ public class RecursiveMethods {
 	public static int[] toReversed(int[] array) {
 		
 			// FIXME create a helper method that can recursively reverse the given array
+		if (array.length == 0) {
 			return new int[0];
-		
+		}
+		else if (array.length == 1) {
+			int[] newArray = new int[1];
+			newArray[0] = array[0];
+			return newArray;
+		}
+		else {
+			int[] reversed = new int[array.length];
+			int lastIndex = array.length - 1;
+			int index = 0;
+			int mirrorIndex = lastIndex - index;
+			return toReversedHelp(array, reversed, index, mirrorIndex);
+		}
+	}
+	
+	public static int[] toReversedHelp(int[] array, int[] reversed, int index, int mirrorIndex) {
+		if (index == array.length / 2) {
+			if (array.length % 2 == 0) {
+				return reversed;
+			}
+			reversed[array.length / 2] = array[array.length / 2];
+			return reversed;
+		}
+		else {
+			reversed[index] = array[mirrorIndex];
+			reversed[mirrorIndex] = array[index];
+			return toReversedHelp(array, reversed, index + 1, mirrorIndex - 1);
+		}
 	}
 
 	/**
@@ -61,6 +105,22 @@ public class RecursiveMethods {
 			double radiusMinimumDrawingThreshold) {
 		
 		// FIXME
+		if (radius <= radiusMinimumDrawingThreshold) {
+			return;
+		}
+		else {
+			StdDraw.circle(xCenter, yCenter, radius);
+		}
+		
+		circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter + radius, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter + radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter - radius, radius/3.0, radiusMinimumDrawingThreshold);
+
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(gcd(48, 14));
 	}
 
 }
